4552fa2576ea80a6ca931aeadb8d50e02b37e28a
SARVESHYOGI <140194642+SARVESHYOGI@users.noreply.github.com>
2024-11-09 18:18:04 +0530

    created login and signup page for main erp

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "../components/app-sidebar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
@@ -28,7 +31,24 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <SidebarProvider>
+          <div className="flex min-h-screen w-full">
+            {/* sidebar left */}
+            <AppSidebar />
+
+            <main className="flex flex-col items-center justify-center min-h-screen w-full">
+              <header className="lg:hidden absolute top-4 left-4 bg-slate-500">
+                <SidebarTrigger
+                  aria-label="Toggle sidebar"
+                  className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-9 w-9 lg:hidden"
+                />
+              </header>
+              <div className="flex flex-col justify-center w-full mx-auto">
+                {children}
+              </div>
+            </main>
+          </div>
+        </SidebarProvider>
       </body>
     </html>
   );
---
+++ components/app-sidebar.tsx
@@ -0,0 +1,67 @@
+import { Home, Inbox } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+// Menu items.
+const items = [
+  {
+    title: "loginformainerp",
+    url: "/loginMainErp",
+    icon: Home,
+  },
+  {
+    title: "signupformainerp",
+    url: "/signUpMainErp",
+    icon: Inbox,
+  },
+  //   {
+  //     title: "Calendar",
+  //     url: "#",
+  //     icon: Calendar,
+  //   },
+  //   {
+  //     title: "Search",
+  //     url: "#",
+  //     icon: Search,
+  //   },
+  //   {
+  //     title: "Settings",
+  //     url: "#",
+  //     icon: Settings,
+  //   },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
