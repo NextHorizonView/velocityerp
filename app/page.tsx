0738fd539749a0483ffad4d4ee4e32dd9624480d
Suyash Labde <suyashlabde123@gmail.com>
2024-11-19 15:45:48 +0530

    solved build error

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,10 @@
-
-import SuperAdminDashboard from "@/components/SuperAdminDashboard";
 import Login from "@/components/Login";
 
 export default function Home() {
   return (
     <>
       <div>
-        
-          {/* <SuperAdminDashboard /> */}
-          <Login/>
-        
+        <Login />
       </div>
     </>
   );
